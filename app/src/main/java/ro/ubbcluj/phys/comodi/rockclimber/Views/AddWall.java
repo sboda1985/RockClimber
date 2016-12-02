dd63b046c15594a7c477bb88fddf484b4a05ef74
Peter <m0uzin@localhost.localdomain>
2016-12-02 23:58:31 +0200

    Ask for necessary fields on saving the wall

--- app/src/main/java/ro/ubbcluj/phys/comodi/rockclimber/Views/AddWall.java
+++ app/src/main/java/ro/ubbcluj/phys/comodi/rockclimber/Views/AddWall.java
@@ -1,12 +1,16 @@
 package ro.ubbcluj.phys.comodi.rockclimber.Views;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import ro.ubbcluj.phys.comodi.rockclimber.R;
+
+import static android.R.string.cancel;
 
 
 public class AddWall extends AppCompatActivity {
@@ -15,6 +19,7 @@ public class AddWall extends AppCompatActivity {
     private EditText gwalldescription;
     private EditText gwallapproach;
     private Button savewallbutton;
+ 
 
 
     @Override
@@ -38,9 +43,50 @@ public class AddWall extends AppCompatActivity {
 
             private void attemptSave() {
 
+                // Reset errors.
+                gwallname.setError(null);
+                gwalldescription.setError(null);
+                gwallapproach.setError(null);
+
+                boolean cancel = false;
+                View focusView = null;
+                 
+                String wall_name = gwallname.getText().toString();
+                String wall_description = gwalldescription.getText().toString();
+                String wall_approach = gwallapproach.getText().toString();
+
+
+
+                // Check for a valid wall  name, if the user entered one.
+                if (TextUtils.isEmpty(wall_name)) {
+                    gwallname.setError(getString(R.string.error_field_required));
+                    focusView = gwallname;
+                    cancel = true;
+                }
+
+                // Check for a valid wall description, if the user entered one.
+                if (TextUtils.isEmpty(wall_description)) {
+                    gwalldescription.setError(getString(R.string.error_field_required));
+                    focusView = gwalldescription;
+                    cancel = true;
+                }
+
+
+                // Check for a valid wall approach, if the user entered one.
+                if (TextUtils.isEmpty(wall_approach)) {
+                    gwallapproach.setError(getString(R.string.error_field_required));
+                    focusView = gwallapproach;
+                    cancel = true;
+                }
+            
+            
             }
+
+            
 
 
         });
     }
+
+    
 }
